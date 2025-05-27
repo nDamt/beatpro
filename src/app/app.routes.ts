69c94ca676912accc73190828b4c46eec6a19580
nDamt <N00242439@upn.pe>
2025-05-27 00:49:07 -0500

    Add Navbar and Footer components with routing support

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,11 @@ import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     {
-        path: 'about',
-        component: AboutComponent,
-    },
-    {
         path: '',
         component: HomeComponent,
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
     }
 ];
